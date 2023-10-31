4804bc5a862570139fa5dad3ea596401eea2b0c8
tharindu <mustythemist@gmail.com>
2023-11-01 02:30:16 +0530

    mobile view v1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import debounce from 'lodash.debounce';
 import './App.css';
-import { data } from './data';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Countries from './components/countries';
-import Layout from './components/layout';
 import CountryInfo from './components/countryInfo';
+import { CiSearch } from 'react-icons/ci'
 
 function App() {
 
@@ -13,7 +12,10 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [activeItem, setActiveItem] = useState(null);
+  const [countryMobileView, setCountryMobileView] = useState(false);
   const [loading, setLoading] = useState(false);
+
+
   const url = 'https://restcountries.com/v3.1/all';
 
   useEffect(() => {
@@ -40,16 +42,17 @@ function App() {
 
   return (
     <div className="App flex-1">
-      <div className='flex  gap-10 mx-auto '>
+      <div className='lg:flex gap-10 mx-auto'>
         <div className='flex-1'>
           <div className='py-10 text-center h-[20vh]'>
             <h1 className='text-5xl text-white'>Country Select</h1>
             <p className="text-slate-200">Select or search for a country for details. </p>
           </div>
-          <div className='text-left'>
-            <input placeholder='Search country name' className='w-full text-white p-4 bg-slate-800 rounded-full mb-8' type="text" onChange={(e) => debouncedSetTest(e.target.value)} />
+          <div className='relative mb-8'>
+            <CiSearch className='absolute top-1/2 -translate-y-1/2 w-14 text-xl' />
+            <input placeholder='Search country name' className='w-full text-white p-4 pl-12 bg-slate-800 rounded-full text-lg' type="text" onChange={(e) => debouncedSetTest(e.target.value)} />
           </div>
-          <div className='flex flex-col gap-4 py-4 h-[calc(70vh-100px)]  overflow-y-auto'>
+          <div className='flex flex-col gap-4 py-4 h-[calc(70vh-100px)] pr-3 overflow-y-auto border-b border-slate-700'>
             <Countries
               countries={countries}
               search={search}
@@ -58,9 +61,11 @@ function App() {
               loading={loading}
             />
           </div>
-          <p className="py-7 text-center h-[10vh] text-slate-300">Copyright © 2023</p>
+          <div className="flex justify-center items-center py-7 h-[10vh] text-slate-300">
+            <span>Copyright © 2023</span>
+          </div>
         </div>
-        <div className='flex-1 flex items-center relative'>
+        <div className={`flex-1 flex items-center lg:relative fixed top-0 left-0 w-full h-screen bg-black lg:bg-transparent ${countryMobileView ? '' : ''}`}>
           <CountryInfo country={selectedCountry} setActiveCountry={setActiveCountry} />
         </div>
       </div>
--- src/components/countryInfo.js
+++ src/components/countryInfo.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import CountryInfoEmpty from './countryInfoEmtry';
+import { AiOutlineClose } from 'react-icons/ai'
 
 const CountryInfo = ({ country, setActiveCountry }) => {
   let currency = null;
@@ -23,20 +24,36 @@ const CountryInfo = ({ country, setActiveCountry }) => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <button className="absolute top-0" onClick={() => setActiveCountry(null, null)}>Close</button>
-          <h3 className='text-4xl mb-8'> {country?.name.common} </h3>
-          <p>Currency Name: {currency[0].name}</p>
-          <p>Currency Symbol: {currency[0].symbol}</p>
-          <p>Flag & Coat of arms</p>
+          <button className="absolute top-[50px] text-xl text-slate-400 left-1/2 -translate-x-1/2 border border-slate-600 p-3 rounded-full hover:text-slate-300 hover:border-slate-400" onClick={() => setActiveCountry(null, null)}><AiOutlineClose /></button>
+          <h3 className='text-4xl mb-9'> {country?.name.common} </h3>
+          <div className='flex justify-center gap-9'>
+            <div className=''>
+              <p className='text-slate-400 uppercase'>Currency</p>
+              <p>{currency[0].name}</p>
+            </div>
+            <div className='border-r border-slate-500'></div>
+            <div className=''>
+              <p className='text-slate-400 uppercase'>Symbol</p>
+              <p className='text-xl'>{currency[0].symbol}</p>
+            </div>
+            <div className='border-r border-slate-500'></div>
+            <div className=''>
+              <p className='text-slate-400 uppercase'>Drives On</p>
+              <p className='capitalize'>{country.car.side}</p>
+            </div>
+          </div>
           <img className="w-[70%] mx-auto  h-[32vh] my-8 object-contain" src={country.flags.svg} alt="" />
           <img className="absolute top-0 w-full h-full object-cover z-[-1] opacity-40 blur-[115px]" src={country.flags.svg} alt="" />
           <div className='h-24'>
             {
-              country.coatOfArms.svg && <img className="w-full h-full object-contain" src={country.coatOfArms.svg} alt="" />
+              country.coatOfArms.svg && (
+                <>
+                  <img className="w-full h-full object-contain" src={country.coatOfArms.svg} alt="" />
+                  <p className='mt-2 text-slate-400 uppercase'>Coat of arms</p>
+                </>
+              )
             }
           </div>
-
-          <p>What side of the road they drive on</p>
         </motion.div>
       }
     </>
